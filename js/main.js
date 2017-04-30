6a7aac36a4cedc1edc17265e448f4cd1a571253a
EDWIN AU <edwinau@rogers.com>
2017-04-30 14:33:48 -0400

    sign in fade out model window upon close

--- js/main.js
+++ js/main.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
   $('.signin').on('click', function(){
     $('.modal').fadeIn("slow");
   });
+
+  $('.close').on('click', function(){
+    $('.modal').fadeOut("3000");
+  });
+
 });
