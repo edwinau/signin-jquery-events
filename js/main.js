d85ab77257ab27a37253fd36de7547838c3dacae
EDWIN AU <edwinau@rogers.com>
2017-04-30 15:48:05 -0400

    e stopPropagation in place

--- js/main.js
+++ js/main.js
@@ -8,12 +8,21 @@ $(document).ready(function() {
     $('.modal').fadeOut("3000");
   });
 
-  $('.submit').on('click', function(){
+  $('.submit').on('click', function(e){
+    e.stopPropagation();
     $('input').addClass('error');
   });
 
   $('input').on('mouseover', function(){
     $('input').removeClass('error');
+  });
+
+  $('input').on('mouseleave', function(){
+    $('input').addClass('error');
+  });
+
+  $('.modal').on('click', function(){
+    $('.modal').fadeOut("slow");
   });
 
 });
